7d28e9f421e1a40b49238dd10aa660a1d7e23942
Ubuntu <ubuntu@ip-172-31-14-26.ec2.internal>
2021-04-27 01:35:25 +0000

    added in hello_world.cpp in proper style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Artur Grigoryev All rights reserved
+//
+// Created by: Artur Grigoryev
+// Created on: Apr 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main() {
+std::cout << "Hello, World!";
+}
